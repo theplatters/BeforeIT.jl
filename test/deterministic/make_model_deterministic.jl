e2c1aaf2f1839b14ba948c0d406b9e9891d3b84f
ameligrana <adrianomeligrana1999@gmail.com>
2026-05-15 11:34:36 +0200

    tests

--- test/deterministic/make_model_deterministic.jl
+++ test/deterministic/make_model_deterministic.jl
@@ -1,32 +1,36 @@
 import BeforeIT as Bit
 
 using Test
-
-import Random: shuffle!, rand, randn
+using Random
 using Distributions
 
-function randn()
+function Random.randn()
     return 0.0
 end
 
-function rand(sp::Bit.WeightVectors.FixedSizeWeightVector)
+function Random.rand(sp::Bit.WeightVectors.FixedSizeWeightVector)
     return findfirst(i -> !iszero(sp[i]), 1:length(sp))
 end
-function rand(n::UnitRange)
+
+function Random.rand(n::UnitRange)
     return 1
 end
-function rand(n::Normal)
+
+function Random.rand(n::Normal)
     return 0.0
 end
+
 function Bit.randpl(n::Int, alpha::Float64, N::Int)
     # return a vector of n numbers that sum to N
     to_return = [Int(round(N / n + 1.0e-7)) for _ in 1:n]
     return to_return
 end
+
 function Bit.epsilon(C::Matrix{Float64})
-    return 0.0, 0.0, 0.0
+    return (0.0, 0.0, 0.0)
 end
-function shuffle!(v::AbstractVector)
+
+function Random.shuffle!(v::AbstractVector)
     # do nothing
 end
 
