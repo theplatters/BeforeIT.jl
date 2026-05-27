c56f5411ddcdada47522e726afe0cc80372502d8
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-05-28 00:51:26 +0200

    Update ecs_utils.jl

--- src/utils/ecs_utils.jl
+++ src/utils/ecs_utils.jl
@@ -28,7 +28,7 @@ macro sum_over(generator)
     end |> esc
 end
 
-single(q::Ark.Query) = Iterators.flatten(zip(tup...) for tup in q) |> only
+single(q::Ark.Query) = getfield.(first(q), 1)
 
 properties(w::Ark.World) = Ark.get_resource(w, Properties)
 expectations(w::Ark.World) = Ark.get_resource(w, Expectations)
